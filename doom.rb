e749eb8b69948d7bc3ea509c0b5f404b3ce77d2c
Daniel Ring <dbr787@gmail.com>
2025-08-02 16:34:10 +1000

    Use HTML width attributes instead of CSS for equal column widths (213+213+214=640)

--- doom.rb
+++ doom.rb
@@ -142,7 +142,7 @@ loop do
     ""
   else
     rows = action_history.map { |entry| "<tr><td class='center'>#{entry[:mode_emoji]}</td><td class='center'>#{entry[:action_emoji]}</td><td class='center'>#{entry[:turn]}</td></tr>" }.join
-    %(<div style="text-align: center;"><table class="mt2" style="width: 640px; margin: 0 auto; display: inline-block;"><colgroup><col style="width: 33.33%"><col style="width: 33.33%"><col style="width: 33.33%"></colgroup><thead><tr><th class='center'>Mode</th><th class='center'>Action</th><th class='center'>Turn</th></tr></thead><tbody>#{rows}</tbody></table></div>)
+    %(<div style="text-align: center;"><table class="mt2" style="width: 640px; margin: 0 auto; display: inline-block;"><thead><tr><th class='center' width="213">Mode</th><th class='center' width="213">Action</th><th class='center' width="214">Turn</th></tr></thead><tbody>#{rows}</tbody></table></div>)
   end
   
   annotate(%(<div class="flex flex-column items-center"><img width="640" height="480" src="artifact://#{i}.png">#{history_table}</div>))
