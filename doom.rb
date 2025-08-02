eaa1f1394f77c291b81119338e42e1aa102f891b
Daniel Ring <dbr787@gmail.com>
2025-08-02 12:10:32 +1000

    Restore HTML width/height attributes for image to maintain original size

--- doom.rb
+++ doom.rb
@@ -145,7 +145,7 @@ loop do
     %(<div style="text-align: center;"><table class="mt2" style="width: 640px; margin: 0 auto; display: inline-block;"><thead><tr><th>Mode</th><th>Move</th></tr></thead><tbody>#{rows}</tbody></table></div>)
   end
   
-  annotate(%(<div class="flex flex-column items-center"><img style="width: 640px; height: 480px;" src="artifact://#{i}.png">#{history_table}</div>))
+  annotate(%(<div class="flex flex-column items-center"><img width="640" height="480" src="artifact://#{i}.png">#{history_table}</div>))
   
   if mode == "random"
     move_input = ask_for_input(i, mode)
